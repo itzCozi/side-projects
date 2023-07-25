0e05e92dfe5e10e9f421130a42b4aeed17714963
Cooper <Cooperransom08@gmail.com>
2023-07-24 21:59:20 -0400

    Make it work!

--- Python/cursesTypeTest.py
+++ Python/cursesTypeTest.py
@@ -4,21 +4,14 @@
 try:
   import os, sys
   import curses
-  import colorama
-  from colorama import Fore, Back, Style
+  import random
+  import requests
 except Exception as e:
   print('Win32 uses a diffrent library, run \'pip install windows-curses\' to fix this.')
   sys.exit(1)
 
 VALID_COLORS = {
-  'black',
-  'red',
-  'green',
-  'yellow',
-  'blue',
-  'magenta',
-  'cyan',
-  'white'
+  'black', 'red', 'green', 'yellow', 'blue', 'magenta', 'cyan', 'white'
 }
 window = curses.initscr()
 window.keypad(True)
@@ -27,6 +20,33 @@ curses.cbreak()
 
 
 class helper:
+
+  def generateSentence():
+    word_site = "https://www.mit.edu/~ecprice/wordlist.10000"
+    response = requests.get(word_site)
+    baselist = []
+    listA = []
+    ticker = -1
+
+    for i in range(random.randint(6, 16)):
+      baselist.append(random.choice(response.content.splitlines()))
+
+    for word in baselist:
+      if len(word) >= 5 and len(listA) < 10:
+        listA.append(word)
+      else:
+        pass
+
+    for item in listA:
+      if isinstance(item, bytes):
+        ticker += 1
+        listA[ticker] = item.decode()
+    first_item = list(listA[0])
+    first_item[0] = first_item[0].upper()
+    listA[0] = ''.join(first_item)
+
+    ret_string = ' '.join(listA)
+    return ret_string + '.'
 
   def EZLog(message):  # Cant print when using curses so...
     if not isinstance(message, str):
@@ -51,33 +71,38 @@ class helper:
       scr.refresh()
       curses.napms(pause)  # Waits the duration of pause in milliseconds
 
-  def writeAndHighlight(scr, text_list, index, color):
-    # NOT WORKING AS OF NOW!!
+  def writeAndHighlight(scr, list, index, color):
     # Writes letters slowley and highlights a char
     # WARN: The 'color' variable does nothing as of now
-    # TODO: Ensure we only highlight the next character not all 'y's in the string
     if color not in VALID_COLORS:
       raise Exception('color variable was given a invaild color')
     else:
       curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)
-    iterated = []
-    
-    for char in range(len(text_list)):
-      if text_list[char] < index and text_list[char] not in iterated:
-        iterated.append(text_list[char]) ; helper.EZLog(str(text_list[char]))
+
+    for char in range(len(list)):
+      if list.index(list[char]) < index and char < index:
         scr.attron(curses.color_pair(1))
-        scr.addstr(char)
+        scr.addstr(list[char])
         scr.attroff(curses.color_pair(1))
       else:
-        scr.addstr(char)
+        scr.addstr(list[char])
       scr.refresh()
 
-  def centeredWrite(scr, text):
+  def centeredWrite(scr, text, slow):
     # Writes to the current line but centers the text
+    # TODO: Make this better and write in the middle of screen not top
+    if not isinstance(slow, bool):
+      raise Exception('slow parameter must be a boolean')
     width = scr.getmaxyx()[1]
     scr.move(scr.getyx()[0], int(width / 2 - len(text) / 2))
-    scr.addstr(text)
-    scr.refresh()
+    if slow == True:
+      for char in text:
+        scr.addstr(char)
+        scr.refresh()
+        curses.napms(20)
+    else:
+      scr.addstr(text)
+      scr.refresh()
 
   def maxSize(window):
     # Returns the width and height of the screen
@@ -124,19 +149,25 @@ def menu(scr):
 
 
 def typeTester(scr):  # The 'PLAY' function
-  test_string = 'Type this if you can.'
-  helper.slowWrite(scr, test_string, 20)
+  scr.clear()
+  string = helper.generateSentence()
+  helper.slowWrite(scr, string, 20)
   loop = True
   ticker = 0
 
   while loop == True:
     key = scr.getch()
-    if key == ord(test_string[ticker]):  # Converts to ASCII
+    if ticker == len(string):
+      scr.clear()
+      helper.centeredWrite(scr, 'YOU DID IT!', True)
+      break
+    elif key == ord(string[ticker]):  # Converts to ASCII
       ticker += 1
       scr.clear()
-      helper.writeAndHighlight(scr, list(test_string), ticker, 'green')
+      helper.writeAndHighlight(scr, list(string), ticker, 'green')
     else:
       continue
+  curses.wrapper(typeTester)
 
 
 curses.wrapper(typeTester)
