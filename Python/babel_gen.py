dbfbc4b560b2ec901c3c70888d0eef4fcfb4c250
Cooper <Cooperransom08@gmail.com>
2023-10-16 22:20:07 -0400

    Follow a little bit of PEP 8 aside from E701

--- Python/babel_gen.py
+++ Python/babel_gen.py
@@ -2,13 +2,12 @@ import os, sys
 import random
 import shutil
 
-
 # CODE
 '''
-* All functions use camelCase and variables use snake_case
 * All variable declarations must be type hinted EX: num: int = 0
 * If a function has parameters each variable must have specified types
-* Functions without any parameters in a class must have the @staticmethod tag
+* All functions and classes use camelCase and variables use snake_case
+* Functions without 'self' parameter in a class must have the @staticmethod tag
 * If a variable or function uses more than one type use EX: num: int | float = 0.1
 -------------- Project Specific --------------
 * A book is a .txt file with 350 pages and 2560 chars per page
@@ -25,8 +24,9 @@ import shutil
 '''
 
 
-class vars:
+class Vars:
 
+  @staticmethod
   def error(error_type: str, var: str = None, type: str = None, runtime_error: str = None) -> None:
     if error_type == 'p':
       print(f'PARAMETER: Given variable {var} is not a {type}.')
@@ -46,9 +46,10 @@ class vars:
   book_pg_count: int = 350
 
 
-class helper:
+class Helper:
 
-  def clearBooksFromVM(rm_zip: bool = False) -> None:  # Deletes all books from VM
+  @staticmethod
+  def clear_books_from_vm(rm_zip: bool = False) -> None:  # Deletes all books from VM
     """
     Used to delete extra books after debugging
 
@@ -56,8 +57,8 @@ class helper:
       rm_zip (bool): If true remove .zip files too
     """
     if not isinstance(rm_zip, bool):
-      vars.error(error_type='p', var='rm_zip', type='boolean')
-      return vars.exit_code
+      Vars.error(error_type='p', var='rm_zip', type='boolean')
+      return Vars.exit_code
 
     for item in os.listdir('/home/runner/dev'):
       if rm_zip is True:
@@ -66,7 +67,8 @@ class helper:
       if item.endswith('.txt'):
         os.remove(item)
 
-  def zipBooks(file_list: list, output_path: str) -> str:
+  @staticmethod
+  def zip_books(file_list: list, output_path: str) -> str:
     """
     Puts items file_list into a zip file
 
@@ -78,13 +80,13 @@ class helper:
       str: The path of the archive
     """
     if not isinstance(file_list, list):
-      vars.error(error_type='p', var='file_list', type='list')
-      return vars.exit_code
+      Vars.error(error_type='p', var='file_list', type='list')
+      return Vars.exit_code
     if not isinstance(output_path, str):
-      vars.error(error_type='p', var='output_path', type='string')
-      return vars.exit_code
+      Vars.error(error_type='p', var='output_path', type='string')
+      return Vars.exit_code
 
-    dump_dir: str = helper.genID()  # Not a case because it's removed after use
+    dump_dir: str = Helper.gen_id()  # Not a case because it's removed after use
     os.mkdir(dump_dir)
     for target in file_list:
       target_file: str = target.split('\\')[-1]
@@ -99,7 +101,8 @@ class helper:
       os.remove(file)
     return zip_file
 
-  def genID(zip: bool = False, lib: bool = False) -> str:
+  @staticmethod
+  def gen_id(zip: bool = False, lib: bool = False) -> str:
     """
     Generates IDs for different file types
 
@@ -111,14 +114,14 @@ class helper:
       str: The ID as a string
     """
     if not isinstance(zip, bool):
-      vars.error(error_type='p', var='zip', type='boolean')
-      return vars.exit_code
+      Vars.error(error_type='p', var='zip', type='boolean')
+      return Vars.exit_code
     if not isinstance(lib, bool):
-      vars.error(error_type='p', var='lib', type='boolean')
-      return vars.exit_code
+      Vars.error(error_type='p', var='lib', type='boolean')
+      return Vars.exit_code
 
     length: int = 14
-    alphabet: list = vars.id_alphabet_list
+    alphabet: list = Vars.id_alphabet_list
     id: list = []
 
     for num in range(length):
@@ -127,20 +130,22 @@ class helper:
     if zip is True:
       for i in range(3):
         id.remove(random.choice(id))
-      id.insert(4, '-') ; id.insert(0, 'ZIP')
+      id.insert(4, '-')
+      id.insert(0, 'ZIP')
     elif lib is True:
       for i in range(3):
         id.remove(random.choice(id))
-      id.insert(4, '-') ; id.insert(0, 'LIB')
+      id.insert(4, '-')
+      id.insert(0, 'LIB')
     else:
       id.insert(7, '-')
     return ''.join(id)
 
 
-class scribe:
+class Scribe:
 
   @staticmethod
-  def make10Books() -> list:
+  def make_10_books() -> list:
     """
     Makes 10 books in current directory
 
@@ -154,29 +159,29 @@ class scribe:
 
     for book in range(10):
       page_num: int = 0
-      book_path: str = f'{helper.genID()}.txt'
+      book_path: str = f'{Helper.gen_id()}.txt'
 
-      for page in range(vars.book_pg_count):
+      for page in range(Vars.book_pg_count):
         page_list: list = []
         char_ticker: int = 0
 
         title_list: list = []
-        title_str: str = ''.join(random.choices(vars.alphabet, k=12))
+        title_str: str = ''.join(random.choices(Vars.alphabet, k=12))
         for i in range(52): title_list.append(' ')
         title_list.insert(int(52 / 2), title_str)
         page_list.append(''.join(title_list))  # Append title to page
 
         line_list: list = []
 
-        for char in range(vars.pg_char_count - len(page_list[0])):
+        for char in range(Vars.pg_char_count - len(page_list[0])):
           char_ticker += 1
           if char_ticker % 64 == 0:
             if len(line_list) != 64:
               for i in range(64 - len(line_list)):
-                line_list.append(random.choice(vars.alphabet))
+                line_list.append(random.choice(Vars.alphabet))
             page_list.append(''.join(line_list))
             line_list: list = []
-          line_list.append(random.choice(vars.alphabet))
+          line_list.append(random.choice(Vars.alphabet))
 
         page_num += 1
         page_char_count: int = len(''.join(page_list))
@@ -187,7 +192,7 @@ class scribe:
           book_list.append(book_path)
           print(
             f'\n* Book "{book_path}" #{book_num} character count: {book_char_count}'
-            f'\n* Book "{book_path}" #{book_num} created with {vars.book_pg_count} pages\n'
+            f'\n* Book "{book_path}" #{book_num} created with {Vars.book_pg_count} pages\n'
           )
           group_char_count: int = 0
         elif page_num % 50 == 0:
@@ -199,7 +204,8 @@ class scribe:
       book_char_count: int = 0
     return book_list
 
-  def generateCases(amount: int) -> None:
+  @staticmethod
+  def generate_cases(amount: int) -> None:
     """
     Zips books together into cases and library's
 
@@ -207,20 +213,20 @@ class scribe:
       amount (int): The amount of cases in the library
     """
     if not isinstance(amount, int):
-      vars.error(error_type='p', var='amount', type='integer')
-      return vars.exit_code
-    if 'linux' in vars.platform and vars.DEV_MODE is True:  # For testing VM
+      Vars.error(error_type='p', var='amount', type='integer')
+      return Vars.exit_code
+    if 'linux' in Vars.platform and Vars.DEV_MODE is True:  # For testing VM
       del_files: str = input('Delete all .txt and .zip files in dev? (y/n): ')
-      if del_files == 'y' or del_files == 'yes': helper.clearBooksFromVM(True)
+      if del_files == 'y' or del_files == 'yes': Helper.clear_books_from_vm(True)
 
     zip_list: list = []
     for book_cycle in range(amount):
-      case_list: list = scribe.make10Books()
+      case_list: list = Scribe.make_10_books()
       if book_cycle + 1 == amount:
         print('----------------------------------------------')
       else:
         print('----------------------------------------------\n')
-      zip_file: str = helper.zipBooks(case_list, helper.genID(zip=True))
+      zip_file: str = Helper.zip_books(case_list, Helper.gen_id(zip=True))
       zip_list.append(zip_file)
 
     for zip in zip_list:
@@ -229,11 +235,12 @@ class scribe:
       file_size: int | float = round(os.path.getsize(zip_list[idx]) / 125000, 2)
       print(f'{idx + 1}. Archive "{file_name}" is {file_size} MB')
 
-    master_zip: str = helper.zipBooks(zip_list, helper.genID(lib=True))
+    master_zip: str = Helper.zip_books(zip_list, Helper.gen_id(lib=True))
     master_size: int | float = round(os.path.getsize(master_zip) / 125000, 2)
     master_name: str = '/'.join(master_zip.split('\\')[-3:])
     print(f'Master archive "{master_name}" is {master_size} MB')
     print('----------------------------------------------')
 
 
-scribe.generateCases(5)
+if __name__ == "__main__":
+  Scribe.generate_cases(5)
