ae4f64d265bc32d85619cf56bcbd1e4e85312259
Cooper <Cooperransom08@gmail.com>
2023-10-17 14:10:10 -0400

    Make sum rushed!!

---
+++ Python/console_emulator.py
@@ -0,0 +1,15 @@
+# Emulates the windows terminal and cmd
+import os
+import sys
+
+
+class Globals:
+  current_working_dir: str = os.getcwd()
+  command_map: dict = {
+    "cd": 1,
+    "ls": 2,
+    "echo": 3,
+    "clear" | "cls": 4,  # i want cls and clear to both be 4
+    
+
+  }
