8407a66c68c9e5a78d4ba7df674e9c4b66b1d09a
Cooper <Cooperransom08@gmail.com>
2023-10-17 14:58:18 -0400

    add 2 commands

--- Python/console_emulator.py
+++ Python/console_emulator.py
@@ -10,6 +10,6 @@ class Globals:
     "ls": 2,
     "echo": 3,
     "clear" | "cls": 4,  # i want cls and clear to both be 4
-    
-
+    "touch": 5,  # Creates a file
+    "rm": 6  # Removes a file
   }
