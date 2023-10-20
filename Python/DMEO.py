f1cb11b628b9cbe95aef14630f3c294275d9db31
Cooper <Cooperransom08@gmail.com>
2023-10-20 16:39:23 -0400

    make both functions work

--- Python/DMEO.py
+++ Python/DMEO.py
@@ -19,14 +19,16 @@ import pyminizip
 
 # TODO
 '''
-* Make the unzip function
+* Make a password zip function so i can put
+the file zips into the password protected
+archive
 * Add doc-strings
 '''
 
 
 def random_id() -> str:
   length: int = 8
-  alphabet: list = list(string.ascii_letters + string.digits)
+  alphabet: list = list(string.ascii_uppercase + string.digits)
   id_list: list = []
   for i in range(length):
     id_list.append(random.choice(alphabet))
@@ -34,49 +36,50 @@ def random_id() -> str:
   return str(''.join(id_list))
 
 
-def zip_file(target: str, archive_name: str, output_path: str = '') -> str:  # Might make this name it self and return the ID
+def zip_file(target: str, output_path: str = '') -> str:
   """
   Zips either one file or one directory
 
   Args:
     target (str): The file or directory to compress
-    archive_name (str): The name of the .zip file
-    output_path (str, optional): The path to the .zip file. Defaults to ''.
+    output_path (str, optional): The path to the '.zip' file. Defaults to ''.
 
   Returns:
     str: The path to the .zip file
   """
-  if archive_name[-4:] == '.zip':
-    archive_name = archive_name[:-4]
-
   cur_dir = os.getcwd().replace('\\', '/')
   if output_path == '': output_path = cur_dir
-  zip_path = f'{output_path}/{archive_name}'
+  zip_path = f'{output_path}/{random_id()}'
   dump_dir = f'{cur_dir}/{random_id()}'
   os.mkdir(dump_dir)
 
   if os.path.isfile(target):
-    with open(target, 'rb') as Fin:
-      content = Fin.read()
-    with open(f'{dump_dir}/{target}', 'wb') as Fout:
-      Fout.write(content)
+    with open(target, 'rb') as file_in:
+      content = file_in.read()
+    with open(f'{dump_dir}/{target}', 'wb') as file_out:
+      file_out.write(content)
   elif os.path.isdir(target):
     shutil.copytree(target, f'{dump_dir}/{target.split("/")[-1]}')
 
   zip_file = shutil.make_archive(zip_path, 'zip', dump_dir)
   shutil.rmtree(dump_dir)
-  return zip_file
+  return zip_file.replace('\\', '/')
 
-def unzip_file(file_path: str, out_dir: str) -> str:
-  # Have to make this work: https://www.geeksforgeeks.org/python-shutil-unpack_archive-method/
-  file_path = file_path.replace('\\', '/')
+
+def unzip_file(file_path: str) -> str:
+  """
+  Unzip an archive to a folder named after it 
+
+  Args:
+    file_path (str): The path to the '.zip' file
+
+  Returns:
+    str: The path to the directory
+  """
   cur_dir = os.getcwd().replace('\\', '/')
-  #os.mkdir(file_path.split("/")[-1][:-4])
-  out_path = f'{cur_dir}/{file_path.split("/")[-1]}'
-
-  shutil.unpack_archive(filename=file_path, format='zip')
-  #return out_path
+  out_dir = f'{cur_dir}/{file_path.split("/")[-1][:-4]}'
+  shutil.unpack_archive(file_path, out_dir, 'zip')
+  return out_dir
 
 
-
-unzip_file('out.zip', 'out')
+zip_file('Coop-OS')
