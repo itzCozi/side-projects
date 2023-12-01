08b3e28e13e9ca51fe09ee2554098f7544fed717
Cooper <Cooperransom08@gmail.com>
2023-12-01 13:21:53 -0500

    add create_tarball

--- source/gunz.py
+++ source/gunz.py
@@ -1,6 +1,7 @@
 # File/Folder compression script -> .exe
 # 7-Zip Path: C:\Program Files\7-Zip
 # 7z cmd examples: https://7ziphelp.com/7zip-command-line | https://www.dotnetperls.com/7-zip-examples
+# tar in windows: https://stackoverflow.com/questions/37595511/how-to-create-tar-file-on-windows-os
 # Types of compression: 7z, zip, gzip, gztar (tb or tarball)
 # EX: ./gunz -7z test/source   (only arg without '-' is the path)
 # EX: ./gunz -gz test.txt      (compresses test.txt with gunzip)
@@ -8,6 +9,7 @@
 import os, sys
 import platform
 import requests
+import tarfile
 import subprocess
 
 # TODO
@@ -46,11 +48,13 @@ class Gunz:
   def arg_handler() -> None:
     try:
       if __file__.endswith('.py'):
-        arg1 = sys.argv[1].lower()
-        arg2 = sys.argv[2]
+        arg1 = sys.argv[1].lower()   # Switch arg
+        arg2 = sys.argv[2]           # File name
+        arg3 = sys.argv[3]           # Parameter
       if __file__.endswith('.exe'):
-        arg1 = sys.argv[0].lower()
-        arg2 = sys.argv[1]
+        arg1 = sys.argv[0].lower()   # Switch arg
+        arg2 = sys.argv[2]           # File name
+        arg3 = sys.argv[3]           # Parameter
     except Exception:
       pass
 
@@ -67,7 +71,7 @@ class Gunz:
           ...
 
         case '-tb' | '-tarball': # Compress a file or folder with 'tape archive'
-          ...
+          Gunz.create_tarball(arg2)
 
         case _:  # Else statement
           print(f'ERROR: The given argument: "{arg1}" is not recognized.')
@@ -77,7 +81,7 @@ class Gunz:
       print('ERROR: Action executed without required permissions.')
       sys.exit(1)
     except Exception as e:
-      print(f'ERROR: A runtime error occurred, is the process running? \n{e}\n')
+      print(f'ERROR: A runtime error occurred \n{e}\n')
       sys.exit(1)
 
   @staticmethod
@@ -109,12 +113,21 @@ class Gunz:
             \n{_7zip_download}'
           )
 
-  def create_tarball(tar_arg: str | list) -> str:
+  def create_tarball(tar_arg: str | list, output_name: str = '') -> str:
     # Makes tarball out of a folder or a list of files
     tar_arg: str = tar_arg.replace('\\', '/')
 
     if os.path.exists(tar_arg):
-      ...
+      if output_name == '':
+        output_name: str = f'{tar_arg.split("/")[-1].replace(" ", "-")}.tar.gz'
+      if '.tar.gz' not in output_name:
+        output_name: str = f'{output_name}.tar.gz'
+
+      if 'linux' in Gunz._Vars.platform:
+        os.system(f'tar czf {output_name} {tar_arg}')  # Convert thi to tarfile
+      else:
+        ...
+
     elif isinstance(tar_arg, list):
       ...
     else:
@@ -122,7 +135,7 @@ class Gunz:
         f'ERROR: Given argument: "{tar_arg}" is not a list of files or a directory path.'
       )
       return Gunz._Var.exit_code  # Dont sys.exit 
-          
+
 
 # Entry point / Anti-import statement
 if __name__ == '__main__':
