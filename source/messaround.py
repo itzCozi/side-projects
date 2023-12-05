5d64bfc007a2138a5a845df5d5783d5f695eb6f0
Cooper <Cooperransom08@gmail.com>
2023-12-05 15:09:36 -0500

    changes

--- source/messaround.py
+++ source/messaround.py
@@ -12,8 +12,9 @@ import time
 
 
 def get_file_size(filename):
-  st = os.stat(filename)
+  st: os.stat = os.stat(filename)
   return st.st_size
+  #return os.path.getsize(filename)
 
 
 def generate_dummy_file(filename, size):
@@ -31,15 +32,15 @@ def get_extension(name):
 
 
 def compress_file(infile, outfile):
-  zf = zipfile.ZipFile(outfile, mode='x', allowZip64=True)
+  zf: zipfile.ZipFile = zipfile.ZipFile(outfile, mode='x', allowZip64=True)
   zf.write(infile, compress_type=zipfile.ZIP_DEFLATED)
   zf.close()
 
 
 def make_copies_and_compress(infile, outfile, n_copies):
-  zf = zipfile.ZipFile(outfile, mode='x', allowZip64=True)
+  zf: zipfile.ZipFile = zipfile.ZipFile(outfile, mode='x', allowZip64=True)
   for i in range(n_copies):
-    f_name = f'{get_filename_without_extension(infile)}-{i}.{get_extension(infile)}'
+    f_name: str = f'{get_filename_without_extension(infile)}-{i}.{get_extension(infile)}'
     shutil.copy(infile, f_name)
     zf.write(f_name, compress_type=zipfile.ZIP_DEFLATED)
     os.remove(f_name)
@@ -47,23 +48,27 @@ def make_copies_and_compress(infile, outfile, n_copies):
 
 
 if __name__ == '__main__':
-  n_levels = int(sys.argv[1])
-  out_zip_file = sys.argv[2]
-  dummy_name = 'dummy.txt'
-  start_time = time.time()
+  n_levels: int = int(sys.argv[1])
+  out_zip_file: str = sys.argv[2].replace('.zip', '') + '.zip'
+  dummy_name: str = 'dummy.txt'
+  level_1_zip: str = '1.zip'
+  decompressed_size: int = 1
+  start_time: float = time.time()
+
   generate_dummy_file(dummy_name, 1)
-  level_1_zip = '1.zip'
   compress_file(dummy_name, level_1_zip)
   os.remove(dummy_name)
-  decompressed_size = 1
+
   for i in range(1, n_levels + 1):
     make_copies_and_compress(f'{i}.zip', f'{i + 1}.zip', 10)
     decompressed_size *= 10
     os.remove(f'{i}.zip')
+
   if os.path.isfile(out_zip_file):
     os.remove(out_zip_file)
   os.rename(f'{n_levels + 1}.zip', out_zip_file)
-  end_time = time.time()
-  print(f'Compressed File Size: {get_file_size(out_zip_file) / 1024.0} KB')
-  print(f'Size After Decompression: {decompressed_size} GB')
-  print(f'Generation Time: {end_time - start_time}s')
+  end_time: float = time.time()
+
+  print(f'Compressed File Size: {round(get_file_size(out_zip_file) / 1024.0, 2)} KB')
+  print(f'Size After Decompression: ~{round(decompressed_size, 2)} GB')
+  print(f'Generation Time: {round(end_time - start_time, 2)} secs')
